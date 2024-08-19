d9262ebad000c8b3f7aa0d6d61df4f68dbe3476b
OJAS-P-JOSHI <studyfor4yrs@gmail.com>
2024-08-20 01:38:27 +0530

    dist n src

---
+++ TS/TS Foundation/src/index.ts
@@ -0,0 +1,10 @@
+// src/index.ts
+function addNumbers(a: number, b: number): number {
+    return a + b;
+  }
+  
+  const num1 = 5;
+  const num2 = 10;
+  
+  console.log(`The sum of ${num1} and ${num2} is:`, addNumbers(num1, num2));
+  
